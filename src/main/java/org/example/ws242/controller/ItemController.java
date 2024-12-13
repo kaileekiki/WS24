77490a4ad2b273636175d77582b8a93ad75c6527
kai <lucas0606@handong.ac.kr>
2024-12-14 02:02:57 +0900

    itemlist

--- src/main/java/org/example/ws242/controller/ItemController.java
+++ src/main/java/org/example/ws242/controller/ItemController.java
@@ -1,19 +1,20 @@
-//package org.example.ws242.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//@Controller
-//@RequestMapping(value="/item")
-//public class ItemController {
-//    @Autowired
-//    private BoardService boardService;
-//    @RequestMapping(value = "/board/list", method = RequestMethod.GET)
-//    public String boardList(Model model) {
-//        model.addAttribute("list", boardService.getBoardList());
-//        return "posts";
-//    }
-//}
+package org.example.ws242.controller;
+
+import org.example.ws242.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/item")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String ItemList(Model model) {
+        model.addAttribute("itemlist", itemService.getItemList());
+        return "main";
+    }
+}
--- src/main/java/org/example/ws242/dao/ItemDAO.java
+++ src/main/java/org/example/ws242/dao/ItemDAO.java
@@ -1,4 +1,19 @@
 package org.example.ws242.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.example.ws242.vo.ItemVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class ItemDAO {
+    @Autowired
+    SqlSession sqlSession;
+
+    public List<ItemVO> getItemList() {
+        List<ItemVO> list = sqlSession.selectList("Item.getItems");
+        return list;
+    }
 }
