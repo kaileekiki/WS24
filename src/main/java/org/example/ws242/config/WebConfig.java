ec271aff41fbbd191505d45007fa1474129e57b3
kai <lucas0606@handong.ac.kr>
2024-12-14 08:07:57 +0900

    subsadd

--- src/main/java/org/example/ws242/config/WebConfig.java
+++ src/main/java/org/example/ws242/config/WebConfig.java
@@ -2,6 +2,8 @@ package org.example.ws242.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/img/**")
                 .addResourceLocations("file:/Users/ijigwang/Desktop/WS24/target/SpringCRUD-1.0-SNAPSHOT/resources/img/");
+    }
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new DateFormatter("yyyy-MM-dd"));
     }
 }
--- src/main/webapp/WEB-INF/views/addsubs.jsp
+++ src/main/webapp/WEB-INF/views/addsubs.jsp
@@ -39,7 +39,7 @@
     </div>
     <div>
         <label for="endDate">End Date:</label>
-        <input type="date" id="endDate" name="startDate" required />
+        <input type="date" id="endDate" name="endDate" required />
     </div>
     <div>
         <button type="submit">상품 등록</button>
