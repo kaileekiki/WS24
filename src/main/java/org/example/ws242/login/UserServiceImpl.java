298c095613fc34ea0e362147ca90987da1cfde55
Dongheon Kim <22300085@handong.ac.kr>
2024-12-14 00:59:23 +0900

    userservice interface 삭제

--- src/main/java/org/example/ws242/login/UserServiceImpl.java
+++ src/main/java/org/example/ws242/login/UserServiceImpl.java
@@ -1,21 +1,20 @@
 package org.example.ws242.login;
 
 import org.example.ws242.dao.UserDAO;
-import org.example.ws242.service.UserService;
 import org.example.ws242.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl {
     @Autowired
     UserDAO userDAO;
     public UserVO getUser(UserVO vo) {
         return userDAO.getUser(vo);
     }
 
-    @Override
     public void addUser(UserVO user) {
         userDAO.insertUser(user);
     }
+
 }
